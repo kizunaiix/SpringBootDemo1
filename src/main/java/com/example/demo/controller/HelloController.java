13e9752c8407b0680c5584dcf02f622215cc36cc
yaodong <yaodong.sun@outlook.com>
2022-08-23 13:22:34 +0200

    现在能用controller返回html页面了

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 //    @ResponseBody
+    /**这个方法只会去templates文件夹里找html文件，文件名是这个方法返回的String*/
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
