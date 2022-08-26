ecb8e1ba5ec8a2cbcbafec2fbec3257643a6d1d2
kizunaiix <yaodong.sun@outlook.com>
2022-08-26 11:15:47 +0200

    。

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
