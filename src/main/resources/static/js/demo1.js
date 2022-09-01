8a1bce4d0a624648649d46596a3fd230f57eb463
kizunaiix <yaodong.sun@outlook.com>
2022-09-01 17:02:06 +0200

    加了个saber，加了个按钮

--- src/main/resources/static/js/demo1.js
+++ src/main/resources/static/js/demo1.js
@@ -1,2 +1,11 @@
 alert("看到我了吗？");
 /**TODO : js开学！*/
+function popAlert1(){
+    x=document.getElementById("AlertButton")        // 找到元素
+    alert("大哥问你话呢，看到我了没？");
+}
+
+function popAlert2(){
+    x=document.getElementById("AlertButton")        // 找到元素
+    alert("这是我大哥");
+}
