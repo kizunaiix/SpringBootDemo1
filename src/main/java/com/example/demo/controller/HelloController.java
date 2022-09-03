2ab57721d624c1727959a2430426958cade18360
yaodong <yaodong.sun@outlook.com>
2022-09-03 21:55:23 +0200

    .

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,22 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping("")
-    public String home(){
+    public String home() {
         return "homePage/index";
     }
 
-//    @ResponseBody
-    /**这个方法只会去templates文件夹里找html文件，文件名是这个方法返回的String*/
+    // @ResponseBody
+    /** 这个方法只会去templates文件夹里找html文件，文件名是这个方法返回的String */
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
-
     @RequestMapping("/test1")
-    public String testing(){
+    public String testing() {
         return "test1";
     }
-
 
 }
