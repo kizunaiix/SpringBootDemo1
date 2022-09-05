d30683d9c9a8a73a232c5b0ad150c9b50573bc1f
yaodong <yaodong.sun@outlook.com>
2022-09-05 21:54:30 +0200

    。

--- src/main/resources/static/js/demo1.js
+++ src/main/resources/static/js/demo1.js
@@ -15,10 +15,19 @@ function popAlert2(){
 //     window.open("");
 // }
 
-function changeImg() {
-    elements= document.getElementsByClassName("changableImg");
-    for(i=0;i<elements.length;i++){
-        elements[i].src = "../static/img/runningChicken.gif";
-    }
-    
+// function changeImg2RunningChicken() {
+//     elements= document.getElementsByClassName("changableImg");
+//     for(i=0;i<elements.length;i++){
+//         elements[i].src = "../static/img/runningChicken.gif";
+//     } 
+// }
+
+function changeImg2RunningChicken() {
+    element= document.getElementById("changeable1");
+    element= document.getElementById("changeable2");
+    element= document.getElementById("changeable3");
+    element= document.getElementById("changeable4");
+    element.src = "../static/img/runningChicken.gif";
+
+   //这里能不能不写四个函数来实现：“点哪个图片就只有那个图片会变化”呢？
 }
