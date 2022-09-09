6b5645e3dbf1a2ae5053a86ae1c6eb1aee57d8f5
yaodong <yaodong.sun@outlook.com>
2022-09-09 11:18:17 +0200

    .

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("")
+    @RequestMapping("/")
     public String home() {
         return "homePage/index";
     }
